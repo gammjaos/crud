c35e42d2fce17a6aba1388bfec875ae63edcc772
gammjaos <gmurillomartinez@gmail.com>
2021-02-26 14:49:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { isEmpty } from 'lodash';
+import { isEmpty, size } from 'lodash';
 import React, { useState } from 'react'
 import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import shortid from 'shortid';
@@ -26,28 +26,48 @@ function App() {
         setTask("")
     }
 
+    const deleteTask =(id) => {
+        const filteredTask = tasks.filter(task => task.id !== id)
+        setTasks(filteredTask)
+    }
+
     return (
         <div className="container mt-5"> 
         <h1>Tareas</h1>
         <hr/>
         <div className="row">
             <div className="col-8">
+                
                 <h4 className="text-center">Lista de tareas</h4>
-                <ul className="list-group">
-                    
-                    {
-                        //Metemos codigo de javascrip en {}
+                {
+                    size(tasks) == 0 ? (
+                        <h4 className="text-center">Aun no hay tareas</h4>
+                    ) : (
+                    <ul className="list-group">
+                        {
+                            //Metemos codigo de javascrip en {}
 
-                        tasks.map((task) => (
-                            <li className="list-group-item" key={task.id}>
-                                <span className="lead">{task.name}</span>
-                                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                                <button className="btn btn-warning btn-sm float-right">Editar</button>
-                            </li>
-                        ))
-                    }
+                            tasks.map((task) => (
+                                <li className="list-group-item" key={task.id}>
+                                    <span className="lead">{task.name}</span>
+                                    <button 
+                                        className="btn btn-danger btn-sm float-right mx-2"
+                                        onClick={() => deleteTask(task.id)}
+                                    >
+                                        Eliminar
+                                    </button>
 
-                </ul>
+                                    <button 
+                                        className="btn btn-warning btn-sm float-right"
+                                    >
+                                        Editar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    )
+                }
             </div>
 
             <div className="col-4">
